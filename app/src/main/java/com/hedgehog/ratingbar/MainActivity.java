821ce77153259846b789e499b6290473d678504f
Jerry <ciwei17@163.com>
2015-06-11 17:03:25 +0800

    android 中评分的控件

---
+++ app/src/main/java/com/hedgehog/ratingbar/MainActivity.java
@@ -0,0 +1,25 @@
+package com.hedgehog.ratingbar;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RatingBar mRatingBar= (RatingBar) findViewById(R.id.ratingbar);
+        mRatingBar.setOnRatingListener(
+                new RatingBar.OnRatingListener() {
+                    @Override
+                    public void onRating(Object bindObject, int RatingCount) {
+                        Toast.makeText(MainActivity.this,"选中的是"+RatingCount+"颗星星",Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+}
